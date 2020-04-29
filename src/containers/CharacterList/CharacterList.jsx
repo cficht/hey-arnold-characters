c1fe71809f98eb5ee6429a1ed7d7dd3a0a9f642d
cficht <chris.ficht@gmail.com>
2020-04-29 16:32:38 -0700

    initial paging

--- src/containers/CharacterList/CharacterList.jsx
+++ src/containers/CharacterList/CharacterList.jsx
@@ -1,17 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import ListItem from '../../components/ListItem/ListItem';
 import { fetchCharacters } from '../../services/heyarnold';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 
 const CharacerList = () => {
   const [characters, setCharacters] = useState([]);
   let { page } = useParams();
+  let history = useHistory();
+
   if(!page) page = 1;
 
   useEffect(() => {
     fetchCharacters(page)
       .then(apiCharacters => setCharacters(apiCharacters));
-  }, []);
+  }, [page]);
+
+  const nextPage = () => {
+    const newNum = Number(page) + Number(event.target.value);  
+    if(newNum > 0) {
+      page = newNum;
+      history.push(`/${page}`);
+    }
+  };
 
   const characterNodes = characters.map(character => {
     return <ListItem key={character._id} {...character} />;
@@ -20,6 +30,8 @@ const CharacerList = () => {
   return (
     <>
       {characterNodes}
+      <button value={-1} onClick={nextPage}>Back</button>
+      <button value={1} onClick={nextPage}>Forward</button>
     </>
   );
 };
