4eb0836e8bde8babdc6ec8bd41df719d62cf0946
cficht <chris.ficht@gmail.com>
2020-04-29 14:09:51 -0700

    implemented React Router on App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { 
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 import CharacterList from '../../containers/CharacterList/CharacterList';
 import Header from '../Header/Header';
 
 export default function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <CharacterList />
-    </>
+      <Switch>
+        <Route exact path="/" component={CharacterList} />
+      </Switch>
+    </Router>
   );
 }
